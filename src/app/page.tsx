f128f4a9603a784f38fae92e0146b946b694587f
jimmy <jimmy-94@hotmail.fr>
2025-07-23 17:05:31 +0200

    🎉 First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-10 bg-white text-black">
+      <h1 className="text-4xl font-bold">First version 👋</h1>
+      <p className="mt-4 text-lg">Coming soon...</p>
+    </main>
+  );
+}
