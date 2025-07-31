4a733993dadcaa086b2f6bc3da56bea525b48e87
jimmy <jimmy-94@hotmail.fr>
2025-07-31 15:05:57 +0200

    🐛 bugfix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 const isProd = process.env.NODE_ENV === "production";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const repo = "jimmmyy.github.io"; // ← nom du repo
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
   images: {
     unoptimized: true,
   },
