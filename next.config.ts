df62db4f911b9e53cf99a922ebb22d7972fe15b0
jimmy <jimmy-94@hotmail.fr>
2025-07-31 12:01:39 +0200

    🐛 Bug fix path next

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const repo = "jimmmyy.github.io"; // ← nom du repo
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
   images: {
     unoptimized: true,
   },
